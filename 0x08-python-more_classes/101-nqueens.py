c33adf7fea8829896db4f69ff6f41ff79688a8d8
GeletaYohannes <88300740+GeletaYohannes@users.noreply.github.com>
2022-06-28 15:17:39 +0300

    commit

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
-""" defines a nqueens"""
-
+# 101-nqueens.py
+"""Solves the N-queens puzzle.
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard.
+Example:
+    $ ./101-nqueens.py N
+N must be an integer greater than or equal to 4.
+Attributes:
+    board (list): A list of lists representing the chessboard.
+    solutions (list): A list of lists containing solutions.
+Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
+where `r` and `c` represent the row and column, respectively, where a
+queen must be placed on the chessboard.
+"""
 import sys
 
 
@@ -121,3 +133,7 @@ if __name__ == "__main__":
     solutions = recursive_solve(board, 0, 0, [])
     for sol in solutions:
         print(sol)
+    print("Found %d solutions" % len(solutions))
+
+    sys.exit(0)
+# End of 101-nqueens.py
