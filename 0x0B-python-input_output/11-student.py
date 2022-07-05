56681bfb57c806e1aa9c04d35b0e20a2ce52fa90
GeletaYohannes <88300740+GeletaYohannes@users.noreply.github.com>
2022-07-05 23:12:05 +0300

    commit

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,32 +1,38 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+# Matias López <3959@holbertonschool.com>
+"""
+11-student.py
+Public method def reload_from_json(self, json):
+that replaces all attributes of the Student instance
+"""
 
 
 class Student:
-    """Representation of a student"""
+    """
+    class student and def, nomb, apellido, age
+    """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """inicialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        """dictionary representation
+        attrs: list of attributes
+        return: dict representation of instance
+        """
+        my_dict = dict()
+        if type(attrs) is list and all(type(x) is str for x in attrs):
+            for x in attrs:
+                if x in self.__dict__:
+                    my_dict.update({x: self.__dict__[x]})
+            return my_dict
+        return self.__dict__.copy()
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+        """
+        Replaces all attributes of the Student instance.
+        """
+        for x in json:
+            self.__dict__.update({x: json[x]})
