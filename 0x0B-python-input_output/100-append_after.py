5c6cf61e855f2559c70a7b93ec8e561db0fb37be
GeletaYohannes <88300740+GeletaYohannes@users.noreply.github.com>
2022-07-05 23:13:21 +0300

    commit

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# Matias López <3959@holbertonschool.com>
+"""
+12-append_after.py
+a function that inserts a line of text to a file,
+after each line containing a specific string
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    with open(filename, mode='r', encoding='utf-8') as f:
+        text = f.readlines()
+        new_text = []
+
+        for line in text:
+            new_text.append(line)
+
+            if search_string in line:
+                new_text.append(new_string)
+
+    with open(filename, mode='w', encoding='utf-8') as nf:
+        for line in new_text:
+            nf.write(line)
