b2e18076fdcfb23c93c63c67442475cf639c7898
GeletaYohannes <88300740+GeletaYohannes@users.noreply.github.com>
2022-07-05 23:11:04 +0300

    commit

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
-"""Contains the clas "Student"""
+# Matias López <3959@holbertonschool.com>
+"""
+9-student.py
+Write a class Student that defines a student by
+"""
 
 
 class Student:
-    """Representation of a student"""
+    """
+    class student and def, nomb, apellido, age
+    """
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        """inicialization"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of a Student instance"""
-        return self.
+        """return a dictionary"""
+        return self.__dict__
